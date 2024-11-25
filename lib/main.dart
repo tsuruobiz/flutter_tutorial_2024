3b8e916fde96e3f5fb59a1fdcaf75fa146b7bf5a
hideki.tsuruoka.fb <hideki.tsuruoka.fb@nttdocomo.com>
2024-11-25 07:37:05 +0000

    Create the ImageSection widget

--- lib/main.dart
+++ lib/main.dart
@@ -177,3 +177,19 @@ class TextSection extends StatelessWidget {
     );
   }
 }
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+  }
+}
