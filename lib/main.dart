86b426ae0c07e6025350333aa92851ca543f412d
hideki.tsuruoka.fb <hideki.tsuruoka.fb@nttdocomo.com>
2024-11-25 11:02:05 +0000

    コメントを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,49 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
-
-// Flutterのレイアウト構築の方法をを学ぶためのサンプルアプリです。
+// Flutterのレイアウト構築の方法を学ぶためのサンプルアプリです。
 // Flutter公式ドキュメントのBuilding layoutsに沿った実装を行っています。
 // ref. https://docs.flutter.dev/ui/layout/tutorial#step-3-implement-the-button-row
+
+import 'package:flutter/material.dart';
+
+// main関数はDartおよびFlutterにおけるエントリーポイントです。mainから処理がはじまります。
+// 返却値が`void`キーワードは返却する値がないことを示します（内部で処理するだけの関数）。
+// =>: Dartの糖衣構文であり、つまり以下の構文と処理は全く同じです。
+// void main() {
+//   runApp(const MyApp());
+// }
+// runApp(): Flutterフレームワークのトップレベル関数。
+// 引数としてウィジェットを受け取り、それを画面にアタッチします。
+// 基本的に、Flutterにユーザーインターフェースのレンダリングを開始するように指示します。
+// つまり、runApp関数を呼び出すことで、Flutterアプリが画面に表示されるようになります。
+// 引数として渡されたウィジェットは、アプリのルートウィジェットとなり、画面全体を構成する要素となります。
+// この例だとMyAppがRoot Widgetとなります。
+void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout demo';
+
+    // MaterialAppは、Googleが開発したデザインシステムであるMaterial Designを使ったアプリを構築するためのウィジェットです。
+    // Material Designはアプリに一貫性のある外観を与えるためのガイドラインを提供します。
+    // MaterialAppは、アプリのルートウィジェットとして使用され、アプリ全体のテーマ、ナビゲーション、ローカライズなどを管理します。
+    // 例) title: アプリのタイトルを指定します。
+    // 例) home: アプリのホーム画面を指定します。
     return MaterialApp(
       title: appTitle,
+
+      // Scaffoldは、Flutterにおける基本的なページレイアウトウィジェットです。
+      // アプリの画面の基本構造を提供し、アプリバー、ボトムナビゲーションバー、ドロワー、浮動アクションボタンといった、一般的なUI要素を簡単に配置するための枠組みを提供します。
+      // 考えてみてください。ほとんどのアプリは、画面上部にアプリバー（タイトルやアイコンなど）、そして画面の中央に主要なコンテンツを表示します
+      // さらに、画面下部にナビゲーションバーがあったり、側面から開くドロワーメニューがあったりすることもあります。
+      // これらの要素を個別に実装するのは面倒ですが、Scaffoldを使うことで、それらをまとめて簡単に配置できるのです。
       home: Scaffold(
+        // AppBarは、Scaffoldウィジェットの一部であり、画面の上部に表示されるバーです。
+        // 通常、アプリのタイトル、ナビゲーションアイコン、アクションボタンなどを含みます。
+        // AppBarは、Material Designのガイドラインに従って設計されており、アプリに一貫性のある外観を与えます。
+        // 例)title: AppBarに表示するタイトルを指定します。
+        // 例)actions: AppBarの右側に表示するアクションボタンのリストを指定します。
         appBar: AppBar(
           title: const Text(appTitle),
         ),
