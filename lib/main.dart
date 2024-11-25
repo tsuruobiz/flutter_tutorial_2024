3b9763023fbed33a9a5b1b00b70f4a6befd1311c
hideki.tsuruoka.fb <hideki.tsuruoka.fb@nttdocomo.com>
2024-11-25 07:34:32 +0000

    Position the buttons with a Row widget

--- lib/main.dart
+++ lib/main.dart
@@ -87,9 +87,27 @@ class ButtonSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Color color = Theme.of(context).primaryColor;
-    return Text(
-      'テキスト',
-      style: TextStyle(color: color),
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
     );
   }
 }
